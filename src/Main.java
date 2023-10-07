b24f234af4f1902c4808ec2b6aa71ec37b1bd9af
Dyd-hdz <146910278+Dyd-hdz@users.noreply.github.com>
2023-10-07 14:39:27 -0600

    Creación de práctica 01. Proyecto Ant

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ * Proyecto en Ant
+ * @author Luis Hernández
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hola mundo desde un proyecto Ant");
+        /**
+         * El proyecto en ant es mas rapido ya que el compilador es en base a la
+         * que esta instalada en la computadora
+         * Las clases siempre deben de empezar con mayúsculas
+         * El camel case se usa para nombrar atributos y metodos
+         */
+    }
+}
